70782602850ffe37373b329735685dd87850bd0f
Jeremiah <s10198398@connect.np.edu.sg>
2021-02-08 14:09:19 +0800

    starting again for category page templates

--- DCHub/UI Controllers/CategoriesViewController.swift
+++ DCHub/UI Controllers/CategoriesViewController.swift
@@ -12,10 +12,6 @@ class CategoriesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        
-
         // Do any additional setup after loading the view.
     }
     
